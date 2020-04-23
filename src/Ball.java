6a07deeb8b2e8d6f124da62f1650196bdef934f2
elloot <elliot.duchek@gmail.com>
2020-04-23 11:19:21 +0200

    make class for ball, implement ball-creating code from sprite

---
+++ src/Ball.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+
+public class Ball {
+    private int xDirection, yDirection;
+    private int[] pixels;
+    private Rectangle boundingBox;
+    private int width, height;
+
+    public Ball (int x, int y, int width, int height, int col) {
+        this.width = width;
+        this.height = height;
+
+        pixels = new int[height*width];
+
+        int thisIsATest = 0;
+
+        int[] xCoordinates = new int[width];
+        int[] yCoordinates = new int[height];
+
+        for (int i = 0; i < xCoordinates.length; i ++) {
+            if (i < width/2) {
+                xCoordinates[i] = ((xCoordinates.length/2 - i) * -1);
+                yCoordinates[i] = ((yCoordinates.length/2 - i) * -1);
+            } else {
+                xCoordinates[i] = i - ((width/2)-1);
+                yCoordinates[i] = i - ((height/2)-1);
+            }
+        }
+
+        for (int pixelsY = 0; pixelsY < height; pixelsY++) {
+            for (int pixelsX = 0; pixelsX < width; pixelsX++) {
+                for (int k = 0; k < 360; k++){
+                    if (Math.signum(xCoordinates[pixelsX]) == -1 && Math.signum(yCoordinates[pixelsY]) == 1) {
+                        if (xCoordinates[pixelsX] >= (Math.cos(Math.toRadians(k))*(width/2)) && yCoordinates[pixelsY] <= (Math.sin(Math.toRadians(k))*(height/2))) {
+                            pixels[thisIsATest] = 0xFFFF00FF;
+                            break;
+                        } else {
+                            pixels[thisIsATest] = 0x00000000;
+                        }
+                    } else if (Math.signum(xCoordinates[pixelsX]) == 1 && Math.signum(yCoordinates[pixelsY]) == 1) {
+                        if (xCoordinates[pixelsX] <= (Math.cos(Math.toRadians(k))*(width/2)) && yCoordinates[pixelsY] <= (Math.sin(Math.toRadians(k))*(height/2))) {
+                            pixels[thisIsATest] = 0xFFFF00FF;
+                            break;
+                        } else {
+                            pixels[thisIsATest] = 0x00000000;
+                        }
+                    } else if (Math.signum(xCoordinates[pixelsX]) == -1 && Math.signum(yCoordinates[pixelsY]) == -1) {
+                        if (xCoordinates[pixelsX] >= (Math.cos(Math.toRadians(k))*(width/2)) && yCoordinates[pixelsY] >= (Math.sin(Math.toRadians(k))*(height/2))) {
+                            pixels[thisIsATest] = 0xFFFF00FF;
+                            break;
+                        } else {
+                            pixels[thisIsATest] = 0x00000000;
+                        }
+                    } else if (Math.signum(xCoordinates[pixelsX]) == 1 && Math.signum(yCoordinates[pixelsY]) == -1) {
+                        if (xCoordinates[pixelsX] <= (Math.cos(Math.toRadians(k))*(width/2)) && yCoordinates[pixelsY] >= (Math.sin(Math.toRadians(k))*(height/2))) {
+                            pixels[thisIsATest] = 0xFFFF00FF;
+                            break;
+                        } else {
+                            pixels[thisIsATest] = 0x00000000;
+                        }
+                    }
+                }
+                thisIsATest++;
+            }
+        }
+    }
+}
